0a626b2c47d378ff32df00739e074c73b6945ae9
Juliano Da Costa Lopes de Carvalho <julianodacosta512@hotmail.com>
2025-04-21 21:10:30 +0000

    Adição do desafio nivel Mestre ao código

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -6,6 +6,7 @@
 // Siga os comentários para implementar cada parte do desafio.
 //Teste larissa
 
+//Formulas para calcular dendidade populacional e pib per capta (Exercicio "desafio aventureiro")
 float calcDensidadePopulacional(int populacao, float area) {
     return populacao / area;
 }
@@ -15,11 +16,18 @@ float calcPibPerCapta(float pib, int populacao) {
     
 }
 
+//Formula para calcular o super poder das cartas (Exercicio "desafio Mestre")
+float calcSuperPoder(float pib, int pontos, unsigned long int populacao, float area, float pibPerCapta, float densidadePopulacional) {
+    return (float) (pontos + populacao + area + pib + pibPerCapta) - densidadePopulacional;
+}
+
 int main() {
     char estadoA, estadoB, codigoA[20], codigoB[20], nomeA[50], nomeB[50];
-    int populacaoA, populacaoB, pontosA, pontosB;
+    int pontosA, pontosB;
+    unsigned long int populacaoA, populacaoB; //Alteração feita à pedido do desafio Mestre.
     float areaA, areaB, pibA, pibB;
-    float densidadePopulacionalA, pibPerCaptaA, densidadePopulacionalB, pibPerCaptaB;
+    float densidadePopulacionalA, pibPerCaptaA, densidadePopulacionalB, pibPerCaptaB; //Dados do desafio aventureiro
+    float SuperPoderA, SuperPoderB;
 
     //Dados da carta A
     printf("Insira os dados da primeira carta \n");
@@ -33,21 +41,23 @@ int main() {
 
     printf("Insira o nome da cidade A: \n");
     fgets(nomeA, 50, stdin);
-
+    
     printf("Insira a população da cidade A: \n");
-    scanf("%i", &populacaoA);
-
+    scanf("%li", &populacaoA);
+    
     printf("Insira a area da cidade A: \n");
     scanf("%f", &areaA);
-
+    
     printf("Insira o PIB da cidade A: \n");
     scanf("%f", &pibA);
-
+    
     printf("Insira a quantidade de pontos turisticos da cidade A: \n");
     scanf("%i", &pontosA);
-
+    
+    //atribuindo valor as variaveis baseado no resultado das funções
     densidadePopulacionalA = calcDensidadePopulacional(populacaoA, areaA);
     pibPerCaptaA = calcPibPerCapta(pibA, populacaoA);
+    SuperPoderA = calcSuperPoder(pibA, pontosA, populacaoA, areaA, pibPerCaptaA, densidadePopulacionalA);
 
     //Dados da carta B
     printf("Insira os dados da segunda carta \n");
@@ -63,7 +73,7 @@ int main() {
     fgets(nomeB, 50, stdin);
 
     printf("Insira a população da cidade B: \n");
-    scanf("%i", &populacaoB);
+    scanf("%li", &populacaoB);
 
     printf("Insira a area da cidade B: \n");
     scanf("%f", &areaB);
@@ -74,15 +84,20 @@ int main() {
     printf("Insira a quantidade de pontos turisticos da cidade B: \n");
     scanf("%i", &pontosB);
 
+    //atribuindo valor as variaveis baseado no resultado das funções
     densidadePopulacionalB = calcDensidadePopulacional(populacaoB, areaB);
     pibPerCaptaB = calcPibPerCapta(pibB, populacaoB);
+    SuperPoderB = calcSuperPoder(pibB, pontosB, populacaoB, areaB, pibPerCaptaB, densidadePopulacionalB);
 
+    //Informações impressas na tela.
     printf("<-------Carta A------>\n");
-    printf("Carta A:\nEstado: %c\nCódigo: %s\nNome da cidade: %sPopulação: %i Habitantes\nÁrea: %.2f Km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Túristicos: %i\nDensidade Populacional: %.2f hab/km²\nPib per Capta: %.2f reais\n\n", estadoA, codigoA, nomeA, populacaoA, areaA, pibA, pontosA, densidadePopulacionalA, pibPerCaptaA);
+    printf("Carta A:\nEstado: %c\nCódigo: %s\nNome da cidade: %sPopulação: %li Habitantes\nÁrea: %.2f Km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Túristicos: %i\nDensidade Populacional: %.2f hab/km²\nPib per Capta: %.2f reais\nSuper Poder: %.2f\n", estadoA, codigoA, nomeA, populacaoA, areaA, pibA, pontosA, densidadePopulacionalA, pibPerCaptaA, SuperPoderA);
     printf("-------------------------------------------- \n");
     printf("<-------Carta B------>\n");
-    printf("Carta B:\nEstado: %c\nCódigo: %s\nNome da cidade: %sPopulação: %i Habitantes\nÁrea: %.2f Km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Túristicos: %i\nDensidade Populacional: %.2f hab/km²\nPib per Capta: %.2f reais\n\n", estadoB, codigoB, nomeB, populacaoB, areaB, pibB, pontosB, densidadePopulacionalB, pibPerCaptaB);
-    
+    printf("Carta B:\nEstado: %c\nCódigo: %s\nNome da cidade: %sPopulação: %li Habitantes\nÁrea: %.2f Km²\nPIB: %.2f bilhões de reais\nNúmero de Pontos Túristicos: %i\nDensidade Populacional: %.2f hab/km²\nPib per Capta: %.2f reais\nSuper Poder: %2.f\n", estadoB, codigoB, nomeB, populacaoB, areaB, pibB, pontosB, densidadePopulacionalB, pibPerCaptaB, SuperPoderB);
+    printf("-------------------------------------------- \n");
+    printf("Comparação estre as cartas %s e %s\nSe o resultado for 1 a carta %s venceu o desafio, se for 0 a carta %s venceu.\n", nomeA, nomeB, nomeA, nomeB);
+    printf("Papulação: %d\nÁrea: %d\nPIB: %d\nPontos Turísticos: %d\nDensidade Populacionar: %d\nPIB per capita: %d\nSuper Poder: %d", (populacaoA > populacaoB), (areaA > areaB), (pibA > pibB), (pontosA > pontosB), (densidadePopulacionalA < densidadePopulacionalB), (pibPerCaptaA > pibPerCaptaB), (SuperPoderA > SuperPoderB));
 
     return 0;
 }
